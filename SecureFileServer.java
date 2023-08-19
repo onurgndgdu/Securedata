4c9cb1da3a18c0a7536d51efa622af8c69620153
gundogduonur <gundogduonur@outlook.com.tr>
2023-08-19 22:41:10 +0300

    added keystore and fileinput

--- SecureFileServer.java
+++ SecureFileServer.java
@@ -2,7 +2,15 @@ import javax.net.ssl.*;
 import java.io.*;
 import java.security.*;
 public class SecureFileServer {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
+
+        int port = 1234;
+
+        char[] keystorePass = "keystorepass".toCharArray();
+        KeyStore keyStore = KeyStore.getInstance("JKS");
+        FileInputStream keyStoreFile = new FileInputStream("serverkeystore.jks");
+        keyStore.load(keyStoreFile,keystorePass);
+
 
     }
 }
