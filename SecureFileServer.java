b2e25afb6e7376d879435319c10389ac569f8393
gundogduonur <gundogduonur@outlook.com.tr>
2023-08-19 22:51:14 +0300

    used sslcontext TLS and added serversocket

--- SecureFileServer.java
+++ SecureFileServer.java
@@ -11,6 +11,16 @@ public class SecureFileServer {
         FileInputStream keyStoreFile = new FileInputStream("serverkeystore.jks");
         keyStore.load(keyStoreFile,keystorePass);
 
+        KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance("SunX509");
+        keyManagerFactory.init(keyStore,keystorePass);
+
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(keyManagerFactory.getKeyManagers(),null,null);
+
+        SSLServerSocketFactory sslServerSocketFactory = sslContext.getServerSocketFactory();
+        SSLServerSocket serverSocket = (SSLServerSocket) sslServerSocketFactory.createServerSocket(port);
+
+
 
     }
 }
