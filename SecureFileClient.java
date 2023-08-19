e129815de3fbecf156faac080555fdb3449c66e5
gundogduonur <gundogduonur@outlook.com.tr>
2023-08-19 22:25:14 +0300

    created server and client classes

---
+++ SecureFileClient.java
@@ -0,0 +1,8 @@
+import javax.net.ssl.*;
+import java.io.*;
+import java.security.*;
+public class SecureFileClient {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ SecureFileServer.java
@@ -0,0 +1,8 @@
+import javax.net.ssl.*;
+import java.io.*;
+import java.security.*;
+public class SecureFileServer {
+    public static void main(String[] args) {
+
+    }
+}
